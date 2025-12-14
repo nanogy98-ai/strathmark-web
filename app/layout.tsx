2210299c6a355f1c282cd65fe92bed4ef84ba50c
Graeme Tudhope <graeme.tudhope@fvl.co.uk>
2025-12-14 03:31:47 +0000

    Revert i18n changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,10 +61,6 @@ export const metadata: Metadata = {
   },
   alternates: {
     canonical: "https://strathmark.com",
-    languages: {
-      "en-GB": "https://strathmark.com",
-      "es-ES": "https://strathmark.com/es",
-    },
   },
 };
 
